e99d9bdba476abe6a75ae830355ea67457f3575e
TengkuAchmad <achmad.tengku@gmail.com>
2023-11-09 21:45:16 +0700

    Repair Build

--- src/Components/BtnLocation.jsx
+++ src/Components/BtnLocation.jsx
@@ -1,40 +1,43 @@
 // LIBRARY IMPORT
-import { React, useState } from "react";
+import { React, useState, useEffect } from "react";
 import { Button } from "react-bootstrap";
 import { FaRedo } from "react-icons/fa";
 import { format } from "date-fns";
 import { utcToZonedTime } from "date-fns-tz";
 
 const BtnLocation = ({ fetchDataAPI }) => {
-  const [timeSync, setTimeSync] = useState("");
+  const [currentTime, setCurrentTime] = useState(new Date());
   const [isLoading, setIsLoading] = useState(false);
 
-  const currentTime = new Date(); // waktu saat ini pada lokasi pengguna
-  const timeZone = "Asia/Jakarta"; // zona waktu Jakarta
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
   const formattedTime = format(
-    utcToZonedTime(currentTime, timeZone),
+    utcToZonedTime(currentTime, "Asia/Jakarta"),
     "dd/MM HH:mm:ss"
   );
 
   const handleSync = () => {
     setIsLoading(true);
     fetchDataAPI();
-    setTimeSync(formattedTime);
     setTimeout(() => setIsLoading(false), 1000);
   };
+
   return (
     <>
       <Button className="rounded-shape" onClick={() => handleSync()}>
-        {isLoading ? (
-          <FaRedo className="ms-2 rotate" />
-        ) : (
-          <>
-            {timeSync}
-            <FaRedo className="ms-2" />
-          </>
-        )}
+        {formattedTime}
+        <FaRedo className={isLoading ? "ms-2 rotate" : "ms-2"} />
       </Button>
     </>
   );
 };
+
 export default BtnLocation;
